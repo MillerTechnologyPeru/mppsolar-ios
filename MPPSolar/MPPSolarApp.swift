3ac946c3cb872865fe0e7848028a3ebf29ebb312
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-04-17 18:15:44 -0500

    Created iOS project

---
+++ MPPSolar/MPPSolarApp.swift
@@ -0,0 +1,19 @@
+//
+//  MPPSolarApp.swift
+//  MPPSolar
+//
+//  Created by Alsey Coleman Miller on 17/04/21.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct MPPSolarApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
