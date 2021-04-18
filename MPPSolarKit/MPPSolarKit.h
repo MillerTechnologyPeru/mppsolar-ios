3670298a11ef534c5041127a5ee30e5bf75987a8
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-04-17 20:30:52 -0500

    Added MPPSolarKit

---
+++ MPPSolarKit/MPPSolarKit.h
@@ -0,0 +1,18 @@
+//
+//  MPPSolarKit.h
+//  MPPSolarKit
+//
+//  Created by Alsey Coleman Miller on 17/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MPPSolarKit.
+FOUNDATION_EXPORT double MPPSolarKitVersionNumber;
+
+//! Project version string for MPPSolarKit.
+FOUNDATION_EXPORT const unsigned char MPPSolarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPPSolarKit/PublicHeader.h>
+
+
