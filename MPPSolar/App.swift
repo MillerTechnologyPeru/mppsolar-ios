ebe8b457c767666162b06a85057df517c2d03b14
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-04-17 19:34:35 -0500

    Added macOS app

--- MPPSolar/App.swift
+++ MPPSolar/App.swift
@@ -5,25 +5,13 @@
 //  Created by Alsey Coleman Miller on 17/04/21.
 //
 
-import Foundation
 import SwiftUI
 
 @main
 struct MPPSolarApp: App {
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onAppear {
-                    let _ = Self.appLaunch
-                }
         }
     }
-}
-
-private extension MPPSolarApp {
-    
-    static let appLaunch: () = {
-        
-    }()
 }
